a4d392af57eee007be15b9dd32388841e45d27f2
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-04-23 23:08:39 +0800

    update

---
+++ XamarinBugs/XamarinBugs/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace XamarinBugs.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<ViewModel_MainPage>();
+        }
+
+        public ViewModel_MainPage MainPage => SimpleIoc.Default.GetInstance<ViewModel_MainPage>();
+    }
+}
---
+++ XamarinBugs/XamarinBugs/ViewModels/ViewModel_MainPage.cs
@@ -0,0 +1,70 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinBugs.ViewModels
+{
+    public class ViewModel_MainPage : ViewModelBase
+    {
+        #region events
+
+        #endregion
+
+        #region vars
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region commands
+
+        #endregion
+
+        #region ctors
+        public ViewModel_MainPage()
+        {
+            InitCommands();
+
+            // used only in UWP & WPF
+            // or anything that supports design time updates
+            if (base.IsInDesignMode)
+            {
+                DesignData();
+            }
+            else
+            {
+                RuntimeData();
+            }
+        }
+        #endregion
+
+        #region command methods
+
+        #endregion
+
+        #region methods
+        void InitCommands()
+        {
+
+        }
+
+        void DesignData()
+        {
+
+        }
+
+        void RuntimeData()
+        {
+
+        }
+
+        public async Task RefreshData()
+        {
+
+        }
+        #endregion
+    }
+}
